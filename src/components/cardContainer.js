df7a649b9f30d6afd4fa621f7986853ab91ec450
nicol29 <nicolascondrea@gmail.com>
2023-02-11 10:29:24 +0000

    Implement random choosing of cards

--- src/components/cardContainer.js
+++ src/components/cardContainer.js
@@ -7,61 +7,73 @@ function CardContainer () {
     {
       name: 'Gold Block',
       image: require('../images/gold.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Cobblestone',
       image: require('../images/cobblestone.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Crafting Table',
       image: require('../images/crafting-table.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Diamond Ore',
       image: require('../images/diamond.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Dirt',
       image: require('../images/dirt.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Endstone',
       image: require('../images/endstone.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Gravel',
       image: require('../images/gravel.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Log',
       image: require('../images/log.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Netherrack',
       image: require('../images/netherrack.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Obsidian',
       image: require('../images/obsidian.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Sand',
       image: require('../images/sand.jpeg'),
+      clicked: false,
       id: uniqid()
     },
     {
       name: 'Wool',
       image: require('../images/wool.jpeg'),
+      clicked: false,
       id: uniqid()
     }
   ])
@@ -81,6 +93,19 @@ function CardContainer () {
     />
   ))
 
+  let scrambleArray = () => {
+    let scrambledArray = [];
+    let arrayToScramble = cards
+
+    while (scrambledArray.length < 12) {
+      let randomIndex = Math.floor(Math.random() * arrayToScramble.length);
+      
+      scrambledArray.push(arrayToScramble[randomIndex]);
+      arrayToScramble.splice(randomIndex, 1);
+    }
+    return scrambledArray
+  }
+
   return (
     <div>
       <header>
@@ -88,7 +113,7 @@ function CardContainer () {
       </header>
 
       <div className="card-container">
-        {cards[Math.floor(Math.random() * cards.length)]}
+        {scrambleArray()}
       </div>
     </div>
   )
